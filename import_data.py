a09d3de6609d32751b327ca969cdd2458b8c44f3
Everton Zanella Alvarenga <eza@pm.me>
2019-10-16 10:07:03 +0200

    importing all years

--- import_data.py
+++ import_data.py
@@ -1,13 +1,13 @@
 import json
 import psycopg2
 
-filename = './data/jairbolsonaro-2019.json'
-data = json.load(open(filename, 'r'))
-
-conn = psycopg2.connect("dbname=twitterdb user=DB_USER_OWNER password=YOUR_DB_PASSWORD")
-cur = conn.cursor()
-
-for d in data:
-    cur.execute("insert into tweets (tweet, content, time) values (%s, %s, %s)", [json.dumps(d), d['full_text'], d['created_at']])
-
-conn.commit()
+for i in range(2010, 2019):
+    filename = './data/jairbolsonaro-{}.json'.format(i)
+    data = json.load(open(filename, 'r'))
+    
+    conn = psycopg2.connect("dbname=twitterdb user=DB_USER_OWNER password=YOUR_DB_PASSWORD")
+    cur = conn.cursor()
+    for d in data:
+        cur.execute("insert into tweets (tweet, content, time) values (%s, %s, %s)", [json.dumps(d), d['full_text'], d['created_at']])
+    
+    conn.commit()
